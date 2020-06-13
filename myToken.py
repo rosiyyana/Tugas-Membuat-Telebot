ae7741ce8bbedfd9b2aac6a3343a412931a2d54a
rosiyyana <63337889+rosiyyana@users.noreply.github.com>
2020-06-13 13:29:53 +0700

    Update myToken.py

--- myToken.py
+++ myToken.py
@@ -1,3 +1,3 @@
 TOKEN = '1276014890:AAGyXuJPE2_Gu1K9HYj-fPffgjh1JRu1SCc'
-
-deskripsi = "💁Hai aku Rosbot sekarang aku adalah asistenmu saat ini\n \nKamu bisa melihat 🧾data siswa dengan cara ketik : /datasiswa \n"
+bantuan = "Anda bisa melihat 🧾data siswa dengan cara ketik 👉🏼 /datasiswa"
+deskripsi = "💁Hai aku Rosbot sekarang aku adalah asistenmu saat ini\n \nBingung gimana cara melihat apa saja yang bisa Rosbot bantu ❔ Yuk ketik 👉🏼 /help\n\n"
